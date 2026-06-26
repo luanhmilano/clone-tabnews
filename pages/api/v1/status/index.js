499e86d3dc0b1b4b921a1f849e14595e3e9c77d0
Luan H. S. Silva <luanhsantos14@gmail.com>
2026-06-26 18:31:29 +0000

    wip[dia-20]: return the endpoints of database status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,27 @@
 import database from "infra/database.js";
 
 export default async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1;");
-  console.log(result.rows);
-  response.status(200).json({ chave: "miau miau nigma" });
+  const updatedAt = new Date().toISOString();
+
+  const dbVersionResponse = await database.query("SELECT version();");
+  const dbVersion = dbVersionResponse.rows[0].version;
+
+  const dbMaxConnections = await database.query("SHOW max_connections;");
+  const maxConnections = Number.parseInt(
+    dbMaxConnections.rows[0].max_connections,
+  );
+
+  const dbName = process.env.POSTGRES_DB;
+  const dbOpenedConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [dbName],
+  });
+  const openedConnections = dbOpenedConnections.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    version: dbVersion,
+    max_connections: maxConnections,
+    opened_connections: openedConnections,
+  });
 }
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,37 @@
-test("Get to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status")
+test("/api/v1/status should return 200", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+});
+
+test("/api/v1/status should return updatedAt", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+
+  const responseBody = await response.json(); // parsing para JSON do response
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+});
+
+test("/api/v1/status should return DB version", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  const responseBody = await response.json();
+
+  expect(responseBody.version).toEqual(
+    "PostgreSQL 16.0 on x86_64-pc-linux-musl, compiled by gcc (Alpine 12.2.1_git20220924-r10) 12.2.1 20220924, 64-bit",
+  );
+});
+
+test("/api/v1/status should return DB max connections", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  const responseBody = await response.json();
+
+  expect(responseBody.max_connections).toEqual(100);
+});
+
+test("/api/v1/status should return DB opened connections", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  const responseBody = await response.json();
+
+  expect(responseBody.opened_connections).toEqual(1);
 });
